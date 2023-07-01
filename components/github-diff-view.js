8670e0b615194aae196f345a33479b4cce114383
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2023-07-01 20:43:09 +0000

    fix: render diff file handle missing

--- components/github-diff-view.js
+++ components/github-diff-view.js
@@ -2,11 +2,10 @@
 class DiffView extends HTMLElement {
     static NAME = "github-diff-view";
 
-    static ATTR_FILE="file"
-    static ATTR_REPO="repo"
-
-    static URL_PARAM_COMMIT_A = "commitA";
-    static URL_PARAM_COMMIT_B = "commitB";
+    static ATTR_COMMIT_A = "commitA";
+    static ATTR_COMMIT_B = "commitB";
+    static ATTR_FILE = "file"
+    static ATTR_REPO = "repo"
 
     constructor() {
         super();
@@ -14,25 +13,30 @@ class DiffView extends HTMLElement {
 
     connectedCallback() {
         const urlParams = new URLSearchParams(window.location.search);
-        const commitA = urlParams.get(DiffView.URL_PARAM_COMMIT_A);
-        const commitB = urlParams.get(DiffView.URL_PARAM_COMMIT_B);
+        const repo = this.getAttribute(DiffView.ATTR_REPO) ?? urlParams.get(DiffView.ATTR_REPO);
+        const file = this.getAttribute(DiffView.ATTR_FILE) ?? urlParams.get(DiffView.ATTR_FILE);
+        const commitA = this.getAttribute(DiffView.ATTR_COMMIT_A) ?? urlParams.get(DiffView.ATTR_COMMIT_A);
+        const commitB = this.getAttribute(DiffView.ATTR_COMMIT_B) ?? urlParams.get(DiffView.ATTR_COMMIT_B);
 
-        if (commitA && commitB) {
-            this.render(commitA, commitB);
+        if (repo && file && commitA && commitB) {
+            this.render(repo, file, commitA, commitB);
         } else {
-            this.innerHTML = `<p>Missing commit parameters</p>`;
+            this.innerHTML = `<p>Missing some parameters</p><pre><code type="json">${JSON.stringify({ repo, file, commitA, commitB }, 2, null)}</code><pre>`;
         }
     }
 
-    render(commitA, commitB) {
-        const repo = this.getAttribute(DiffView.ATTR_REPO);
-        const file = this.getAttribute(DiffView.ATTR_FILE);
-
-        fetch(`https://api.github.com/repos/${repo}/compare/${commitA}...${commitB}/files/${file}`)
+    render(repo, file, commitA, commitB) {
+        const commitHeadBase = `${commitA}...${commitB}`;
+        const diffApiUrl = `https://api.github.com/repos/${repo}/compare/${commitHeadBase}`;
+        fetch(diffApiUrl)
             .then(response => response.json())
             .then(data => {
-                const diffHtml = data.patch;
-                this.innerHTML = `<pre>${diffHtml}</pre>`;
+                const diffHtml = data.files.filter(f => f.filename === file)[0]?.patch;
+                if (diffHtml) {
+                    this.innerHTML = `<pre>${diffHtml}</pre>`;
+                } else {
+                    throw `Missing '${file}' diff: ${diffApiUrl}`
+                }
             })
             .catch(error => {
                 console.error(error);
