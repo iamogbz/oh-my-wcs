58558d310cb2a03f2034dfd5354eca3b072a810d
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2024-09-16 20:59:42 -0400

    feat: add carousel stack

---
+++ components/carousel-stack.js
@@ -0,0 +1,183 @@
+class CarouselStackElement extends HTMLElement {
+  static NAME = "carousel-stack";
+  static ATTR_DELIM = "|";
+  static attrs = Object.freeze({
+    /** Transition time when images are rotated. */
+    TRANSITION_TIME_SECS: "transition-secs",
+    /** List of image sources e.g. href or data urls, delimited by '|' */
+    IMAGES: "images",
+    IMAGE_GAP: "image-gap",
+    IMAGE_INDEX: "image-idx",
+    /** Style attributes to copy delimted by '|' */
+    STYLE_TRANSFER: "style-transfer",
+  });
+
+  static imagePrefixes = ["in", "bottom", "next", "top", "out"];
+
+  /** @type {Readonly<(typeof CarouselStackElement.attrs)[keyof (typeof CarouselStackElement.attrs)][]>} */
+  static observedAttributes = Object.freeze(
+    Object.values(CarouselStackElement.attrs)
+  );
+
+  constructor() {
+    super();
+    this._root = this.attachShadow({ mode: "open" });
+    this.lastIndex = this.currentIndex;
+    this.createStack();
+  }
+
+  get currentIndex() {
+    return Number(
+      this.getAttribute(CarouselStackElement.attrs.IMAGE_INDEX) || 0
+    );
+  }
+
+  /**
+   * @type {(keyof CSSStyleDeclaration)[]}
+   */
+  get transferStyles() {
+    // @ts-expect-error string vs CSSStyleDeclaration
+    return (
+      this.getAttribute(CarouselStackElement.attrs.STYLE_TRANSFER)?.split(
+        CarouselStackElement.ATTR_DELIM
+      ) ?? []
+    );
+  }
+
+  get images() {
+    return (
+      this.getAttribute(CarouselStackElement.attrs.IMAGES)?.split(
+        CarouselStackElement.ATTR_DELIM
+      ) ?? []
+    );
+  }
+
+  get imageGap() {
+    return this.getAttribute(CarouselStackElement.attrs.IMAGE_GAP);
+  }
+
+  get transitionTimeSecs() {
+    return Number(
+      this.getAttribute(CarouselStackElement.attrs.TRANSITION_TIME_SECS) || 0.3
+    );
+  }
+
+  get transitionTimeMs() {
+    return this.transitionTimeSecs * 1000;
+  }
+
+  createStack() {
+    this.onEachImage((imageElem) => {
+      this._root?.appendChild(imageElem);
+    });
+  }
+
+  connectedCallback() {
+    this.updateStack();
+  }
+
+  attributeChangedCallback() {
+    if (this.lastIndex !== this.currentIndex) {
+      this.startTransition();
+    }
+  }
+
+  adoptedCallback() {
+    this.updateStack();
+  }
+
+  startTransition() {
+    this.onEachImage((imageElem, idx) => {
+      this.setImageOffset(imageElem, idx + 1);
+      // transparent if top two images
+      imageElem.style.opacity = Number(!(idx === 3 || idx === 4)).toString();
+    });
+    setTimeout(() => {
+      this.onEachImage((imageElem) => {
+        imageElem.style.transition = "none";
+      });
+      this.updateStack();
+    }, this.transitionTimeMs * 1.5);
+  }
+
+  updateStack() {
+    const imagesToDisplay = this.images;
+
+    if (imagesToDisplay.length < 1) {
+      // no images to display
+      return;
+    }
+
+    // TODO: double check functionality when more than 3 images provided
+    while (imagesToDisplay.length < 3) {
+      imagesToDisplay.push(...imagesToDisplay);
+    }
+
+    const topImageIdx = this.currentIndex;
+    const nextIdx = (idx = 0, increment = 1) =>
+      (idx + increment) % imagesToDisplay.length;
+
+    // Set image backgrounds and position
+    this.onEachImage((imageElem, idx) => {
+      const imageBg =
+        imagesToDisplay[nextIdx(topImageIdx, imagesToDisplay.length * 2 - idx)];
+      imageElem.style.backgroundImage = imageBg;
+      imageElem.style.position = "absolute";
+      // transparent if top or bottom image
+      imageElem.style.opacity = Number(!(idx === 0 || idx === 4)).toString();
+      this.setImageOffset(imageElem, idx);
+      setTimeout(() => {
+        imageElem.style.transition = ["top", "left", "opacity"]
+          .map((p) => `${p} ${this.transitionTimeSecs}s ease-in-out`)
+          .join(",");
+      }, 0);
+    });
+
+    this.lastIndex = this.currentIndex;
+  }
+
+  /**
+   * Set the position offset of the image based on index.
+   * @param {HTMLElement} imageElem
+   * @param {number} idx
+   */
+  setImageOffset(imageElem, idx) {
+    const posOffset = `calc(${this.imageGap} * (3 - ${idx}))`;
+    imageElem.style.top = posOffset;
+    imageElem.style.left = posOffset;
+  }
+
+  /**
+   * Call a function on each image in the carousel stack
+   * @param {(imageElem: HTMLElement, index: number) => void} callback
+   */
+  onEachImage(callback = () => undefined) {
+    CarouselStackElement.imagePrefixes.forEach((imgPrefix, idx) => {
+      const imageId = `${imgPrefix}Image`;
+      const imageElem =
+        this._root.getElementById(imageId) ?? document.createElement("div");
+      imageElem.id = imageId;
+      this.copyStyles(this, imageElem);
+      callback(imageElem, idx);
+    });
+  }
+
+  /**
+   * Copy all styles from one element to another
+   * @param {HTMLElement} from
+   * @param {HTMLElement} to
+   */
+  copyStyles(from, to) {
+    for (const styleAttr of this.transferStyles) {
+      try {
+        // @ts-expect-error skip readonly property errors
+        to.style[styleAttr] = from.style[styleAttr];
+      } catch (e) {
+        continue;
+      }
+    }
+  }
+}
+
+// Define the custom element
+customElements.define(CarouselStackElement.NAME, CarouselStackElement);
---
+++ components/carousel-stack.md
@@ -0,0 +1,23 @@
+# Carousel Stack
+
+Rotate between images in a specific form
+
+| Param            | Value              | Description                                                                          |
+| ---------------- | ------------------ | ------------------------------------------------------------------------------------ |
+| `component-name` | `"carousel-stack"` | **`CONSTANT`**                                                                       |
+| `display-secs`   | `number`           | Time to display each image.                                                          |
+| `images`         | `string`           | List of image sources e.g. href or data urls, delimited by `\|`.                     |
+| `image-gap`      | `string`           | Gap space between stacked images as css dimension value e.g. `4px`, `1em` etc.       |
+| `image-idx`      | `number`           | The current image index, can be incremented (only) to rotate between images.         |
+| `style-transfer` | `number`           | List of style elements to transfer to each image element in stack, delimted by `\|`. |
+
+## Example
+
+```js
+<script src="https://cdn.jsdelivr.net/gh/iamogbz/oh-my-wcs@main/components/carousel-stack.js"></script>
+
+<carousel-stack images="linear-gradient(red, red)|linear-gradient(green, green)|linear-gradient(blue, blue)"
+    style="width:200px;height:120px;" image-gap="40px" image-idx="0"></carousel-stack>
+```
+
+[Live demo](https://codepen.io/iamogbz/pen/eYwwOMy)
